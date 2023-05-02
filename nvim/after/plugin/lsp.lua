8edc29900675241ccf707b8ccc64d88220b93d02
christof <christof.sirk@gmail.com>
2023-05-02 20:03:32 +0200

    Install lsp-zero

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,40 @@
+local lsp = require('lsp-zero').preset({
+})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.setup_servers({
+  'tsserver',
+  'eslint',
+})
+
+-- Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+
+lsp.setup()
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local cmp_select_opts = {behavior = cmp.SelectBehavior.Select}
+
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+ 
+    -- Navigate between completions
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select_opts),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select_opts),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }
+})
